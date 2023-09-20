af28c88b9180d113fd5cb761b1f0ab0a9adf5a68
Fugazzii <sichinavailia@gmail.com>
2023-09-20 21:20:19 +0400

    Finished main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "./Store/Store.h"
+#include "./Command/Command.h"
 
 using namespace std;
 
 int main() {
+    Store* spar = new Store(1000);
+    Command* command_handler = new Command(spar);
+    string user_input;
+
+    while (true) {
+        cout << ">> ";
+        getline(cin, user_input);
+        command_handler->execute(user_input);
+        cout << "Command executed!\n";
+    }
+
+    delete spar;
 
     return 0;
 }
